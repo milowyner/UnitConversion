c3c5448854273e9a58ae75647f5b18bb7e1f0742
Milo Wyner <milowyner@gmail.com>
2021-07-01 13:48:26 -0700

    Create basic interface for length conversion with input text field, input unit segmented control, output text label, and output unit segmented control

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputLength = ""
+    @State private var inputUnit = 0
+    @State private var outputUnit = 0
+    
+    var outputLength: Double {
+        guard let inputLength = Double(inputLength) else { return 0 }
+        return inputLength
+    }
+    
+    let units = ["m", "km", "ft", "yrd", "mile"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section(header: Text("Input")) {
+                TextField("Length", text: $inputLength)
+                Picker("Length unit", selection: $inputUnit) {
+                    ForEach(0..<units.count) {
+                        Text(units[$0])
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("Output")) {
+                Text("\(outputLength)")
+                Picker("Length unit", selection: $outputUnit) {
+                    ForEach(0..<units.count) {
+                        Text(units[$0])
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+        }
     }
 }
 
